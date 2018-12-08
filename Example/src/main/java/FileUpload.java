23c5101a691c98c4561bf978ca480a4f2c70b60e
sureshdagani <37266523+sureshdagani@users.noreply.github.com>
2018-12-08 22:17:54 +0530

    Add files via upload

---
+++ Example/src/main/java/FileUpload.java
@@ -0,0 +1,60 @@
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileUpload {
+
+	String URL = "https://pdf2doc.com/";
+	@Test
+	public void testUpload() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\chrome\\chromedriver.exe");
+		WebDriver  driver = new ChromeDriver();
+		driver.get(URL);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		
+		WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Upload Files')]"));				
+		element.click();
+		// path to the file
+		Thread.sleep(2000);
+		uploadFile("D:\\Test.pdf");
+		Thread.sleep(2000);
+		
+		driver.quit();
+	}
+	
+	/**
+     * This method will set any parameter string to the system's clipboard.
+     */
+	public static void setClipboardData(String string) {
+		//StringSelection is a class that can be used for copy and paste operations.
+		   StringSelection stringSelection = new StringSelection(string);		   
+		   Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+		}
+	
+	public static void uploadFile(String fileLocation) {
+        try {
+        	//Setting clipboard with file location
+            setClipboardData(fileLocation);
+            //native key strokes for CTRL, V and ENTER keys            
+            Robot robot = new Robot();
+	
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);            
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);            
+        } catch (Exception exp) {
+        	exp.printStackTrace();
+        }
+    }
+}
